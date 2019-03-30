ca8f24215bb688695754c0ea69feb0cb7a85e46a
power3853 <power3853@naver.com>
2019-03-30 22:59:24 +0900

    adding index file1

--- src/main/java/com/sadang/app/WorkoutTracker/WorkoutTrackerApplication.java
+++ src/main/java/com/sadang/app/WorkoutTracker/WorkoutTrackerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @Controller
@@ -14,7 +15,10 @@ public class WorkoutTrackerApplication {
 	}
 
 	@GetMapping("")
-	public String hello() {
-		return "index";
+	public ModelAndView hello() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject( "name", "HS" );
+		modelAndView.setViewName("index");
+		return modelAndView;
 	}
 }
